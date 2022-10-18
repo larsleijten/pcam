09a556cc0c50490387979f136a74faa0d543d391
larsleijten <lars@13a.nl>
2022-10-18 14:59:53 +0200

    Created a network class and functions

---
+++ convnet.py
@@ -0,0 +1,89 @@
+class ConvNet(nn.Module):
+  def __init__(self):
+    super().__init__()
+    # Build a small Convolutional Neural Network
+    self.conv = nn.Sequential(
+        nn.Conv2d(3,32,kernel_size=3,padding=1),
+        nn.ReLU(),
+        nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2,2),
+
+        nn.Conv2d(64,128,kernel_size=3,stride=1,padding=1),
+        nn.ReLU(),
+        nn.Conv2d(128,128,kernel_size=3,stride=1,padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(4,4),
+
+        nn.Conv2d(128,256,kernel_size=3,stride=1,padding=1),
+        nn.ReLU(),
+        nn.Conv2d(256,256,kernel_size=3,stride=1,padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(4,4),
+
+        nn.Flatten(),
+        nn.Linear(2304,1024),
+        nn.ReLU(),
+        nn.Linear(1024,512),
+        nn.ReLU(),
+        nn.Linear(512,1)
+    )
+  
+  def forward(self, x):
+    logit = self.conv(x)
+    #p = nn.Sigmoid(logit)
+    return logit
+
+
+
+def train_convnet(dataloader, model, loss_fn, optimizer):
+  size = len(dataloader.dataset)
+  model.train()
+  for batch, (X, y) in enumerate(dataloader):
+      # Make sure the tensors are set to be processed by the correct device
+      X, y = X.to(device), y.to(device)
+
+      # Compute prediction error
+      pred = model(X)
+      loss = loss_fn(pred, y.unsqueeze(1).float())
+      # Backpropagation
+      # This prevents gradients from adding up
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+      if batch % 100 == 0:
+          loss, current = loss.item(), batch * len(X)
+          print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def validation_convnet(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y.unsqueeze(1).float()).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Validation Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+def test_convnet(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y.unsqueeze(1).float()).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
