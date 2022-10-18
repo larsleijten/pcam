0bdec319817b391c60a843129a5eea9589004db2
larsleijten <lars@13a.nl>
2022-10-18 15:29:52 +0200

    pass device to functions

--- convnet.py
+++ convnet.py
@@ -51,7 +51,7 @@ class ConvNet(nn.Module):
 
 
 
-def train_convnet(dataloader, model, loss_fn, optimizer):
+def train_convnet(dataloader, model, loss_fn, optimizer, device):
   size = len(dataloader.dataset)
   model.train()
   for batch, (X, y) in enumerate(dataloader):
@@ -72,7 +72,7 @@ def train_convnet(dataloader, model, loss_fn, optimizer):
           print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def validation_convnet(dataloader, model, loss_fn):
+def validation_convnet(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -88,7 +88,7 @@ def validation_convnet(dataloader, model, loss_fn):
     print(f"Validation Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-def test_convnet(dataloader, model, loss_fn):
+def test_convnet(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
